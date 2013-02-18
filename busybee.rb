84e81dfa805066f069804a78321adfee666aae33
Sean Ogden <positivelyskewed.sf@gmail.com>
2013-02-18 13:05:46 -0500

    fix signal behavior in busybee.

--- busybee.rb
+++ busybee.rb
@@ -30,10 +30,29 @@ class Busybee < Formula
 end
 __END__
 diff --git a/busybee.cc b/busybee.cc
-index ede42bf..94595be 100644
+index ede42bf..8f2f0ff 100644
 --- a/busybee.cc
 +++ b/busybee.cc
-@@ -1712,12 +1712,19 @@ CLASSNAME :: wait_event(int* fd, uint32_t* events)
+@@ -520,13 +520,15 @@ void
+ CLASSNAME :: add_signals()
+ {
+ #ifdef HAVE_KQUEUE
+-    struct kevent ee[3];
++    struct kevent ee[5];
+     memset(&ee, 0, sizeof(ee));
+     EV_SET(&ee[0], SIGTERM, EVFILT_SIGNAL, EV_ADD | EV_CLEAR, 0, 0, NULL);
+-    EV_SET(&ee[1], SIGQUIT, EVFILT_SIGNAL, EV_ADD | EV_CLEAR, 0, 0, NULL);
++    EV_SET(&ee[1], SIGHUP, EVFILT_SIGNAL, EV_ADD | EV_CLEAR, 0, 0, NULL);
+     EV_SET(&ee[2], SIGINT, EVFILT_SIGNAL, EV_ADD | EV_CLEAR, 0, 0, NULL);
++    EV_SET(&ee[3], SIGALRM, EVFILT_SIGNAL, EV_ADD | EV_CLEAR, 0, 0, NULL);
++    EV_SET(&ee[4], SIGUSR1, EVFILT_SIGNAL, EV_ADD | EV_CLEAR, 0, 0, NULL);
+ 
+-    if(kevent(m_epoll.get(), ee, 3, NULL, 0, NULL) < 0)
++    if(kevent(m_epoll.get(), ee, 5, NULL, 0, NULL) < 0)
+     {
+         throw po6::error(errno);
+     }
+@@ -1712,24 +1714,42 @@ CLASSNAME :: wait_event(int* fd, uint32_t* events)
  {
      struct kevent ee;
      struct timespec to = {0,0}; 
@@ -55,4 +74,36 @@ index ede42bf..94595be 100644
 -    int ret = kevent(m_epoll.get(), NULL, 0, &ee, 1, &to);
      *fd = ee.ident;
  
-     switch(ee.filter)
+-    switch(ee.filter)
++    if (ret > 0)
+     {
+-        case EVFILT_READ:
+-            *events = EPOLLIN;
+-            break;
+-        case EVFILT_WRITE:
+-            *events = EPOLLOUT;
+-            break;
+-        default:
+-            *events = EPOLLERR;
++        switch(ee.filter)
++        {
++            case EVFILT_READ:
++                *events = EPOLLIN;
++                break;
++            case EVFILT_WRITE:
++                *events = EPOLLOUT;
++                break;
++            case EVFILT_SIGNAL:
++                sigset_t origmask;
++                sigprocmask(SIG_SETMASK, &m_sigmask, &origmask);
++                kill(getpid(), ee.ident); 
++                sigprocmask(SIG_SETMASK, &origmask, NULL);
++                ret = -1;
++                errno = EINTR;
++                break;
++            default:
++                *events = EPOLLERR;
++        }
+     }
+ 
+     return ret;
