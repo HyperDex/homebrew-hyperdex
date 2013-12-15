bce5c56d376a0baae9cde13ad88db9d1fe8e7469
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-15 10:49:55 +0000

    replicant: update to newest release.

--- replicant.rb
+++ replicant.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Replicant < Formula
   homepage 'http://hyperdex.org/'
-  url 'http://hyperdex.org/src/replicant-0.1.2.tar.gz'
-  sha1 '9de930aa87321b5537f66745206ffc4e153c20fa'
+  url 'http://hyperdex.org/src/replicant-0.3.1.tar.gz'
+  sha1 'ba8022e40568ebcc6e56e80b7d7119694c18ad01'
 
   depends_on 'autoconf'
   depends_on 'autoconf-archive'
   depends_on 'libtool'
   depends_on 'automake'
-  depends_on 'leveldb'
+  depends_on 'hyperleveldb'
   depends_on 'cityhash'
   depends_on 'popt'
   depends_on 'glog'
@@ -19,7 +19,6 @@ class Replicant < Formula
     ENV.delete 'LD'
     ENV['CC']="#{HOMEBREW_PREFIX}/bin/gcc-4.7"
     ENV['CXX']="#{HOMEBREW_PREFIX}/bin/g++-4.7"
-    system "curl -O https://raw.github.com/rescrv/Replicant/master/memstream.h"
     system "autoreconf -if"
     system "./configure", "--prefix=#{prefix}", "PO6_LIBS=-L#{HOMEBREW_PREFIX}/lib",
             "PO6_CFLAGS=-I#{HOMEBREW_PREFIX}/include", "E_LIBS=\"-L#{HOMEBREW_PREFIX}/lib -le\"",
