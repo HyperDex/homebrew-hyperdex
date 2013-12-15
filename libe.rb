02ed092974f5c102cf6a858f76faf6e5eab96b7a
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-15 10:49:22 +0000

    libe: update to newest release.

--- libe.rb
+++ libe.rb
@@ -2,15 +2,10 @@ require 'formula'
 
 class Libe < Formula
   homepage 'http://hyperdex.org'
-  url 'http://hyperdex.org/src/libe-0.3.1.tar.gz'
-  sha1 '40754b1a9acdbd8154b5e4e348a74ff2550022bb'
+  url 'http://hyperdex.org/src/libe-0.4.1.tar.gz'
+  sha1 'e30607fbc4f028c1c9952c677eea72e83b31da96'
 
   depends_on 'libpo6'
-
-  def patches
-    # fix up build error.
-    DATA
-  end
 
   def install
     ENV.delete 'LD'
@@ -23,17 +18,3 @@ class Libe < Formula
     system "make install"
   end
 end
-__END__
-diff -Naur libe-0.3.1/time.cc libe-0.3.1-fix/time.cc
---- libe-0.3.1/time.cc	2013-02-12 11:06:47.000000000 -0500
-+++ libe-0.3.1-fix/time.cc	2013-02-16 21:52:32.000000000 -0500
-@@ -69,7 +69,7 @@
-     QueryPerformanceFrequency((LARGE_INTEGER*)&tickfreq);
-     QueryPerformanceCounter((LARGE_INTEGER*)&timestamp);
-     return timestamp.QuadPart / (tickfreq.QuadPart/1000000000.0);
--#elif defined HAVE_MACH_TIMEBASE_INFO
-+#elif defined HAVE_MACH_ABSOLUTE_TIME
-     mach_timebase_info_data_t info;
-     mach_timebase_info(&info);
-     return mach_absolute_time()*info.numer/info.denom;
-
