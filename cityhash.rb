2096fa7f5719a60982925d2660499847676435b0
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-15 10:49:07 +0000

    cityhash: update to newest release.

--- cityhash.rb
+++ cityhash.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Cityhash < Formula
   homepage 'http://code.google.com/p/cityhash/'
-  url 'http://cityhash.googlecode.com/files/cityhash-1.0.3.tar.gz'
-  sha1 '855c1aa88267fb30a8a9ecd849bb2838638e2248'
+  url 'http://cityhash.googlecode.com/files/cityhash-1.0.1.tar.gz'
+  sha1 '850c68afb47b3743e7884809cf67b98f9a6db876'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
