0635651511ae633bd62b3f9b3c662ea89d094b2c
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-16 03:58:17 +0000

    hyperleveldb: rename formula.

--- hyperleveldb.rb
+++ hyperleveldb.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class HyperLevelDB < Formula
+class Hyperleveldb < Formula
   homepage 'http://github.com/rescrv/HyperLevelDB'
   url 'http://hyperdex.org/src/hyperleveldb-1.0.1.tar.gz'
   sha1 '12ba2b093a4a67f04edaaeaf3f1db7943d0a3147'
