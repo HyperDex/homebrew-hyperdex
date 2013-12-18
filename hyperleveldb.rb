45b050aa9e1425373e9306855157752e03505b26
Seyi Ogunyemi <micrypt@gmail.com>
2013-12-18 16:55:50 +0000

    hyperleveldb: add GCC.

--- hyperleveldb.rb
+++ hyperleveldb.rb
@@ -5,6 +5,8 @@ class Hyperleveldb < Formula
   url 'http://hyperdex.org/src/hyperleveldb-1.0.1.tar.gz'
   sha1 '12ba2b093a4a67f04edaaeaf3f1db7943d0a3147'
 
+  depends_on 'homebrew/versions/gcc48' => :build
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
