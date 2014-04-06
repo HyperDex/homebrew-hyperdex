fe0d8d3856b5eed82895c41d5b6436f4500c20e6
Robert Escriva <me@robescriva.com>
2014-04-06 10:09:33 -0400

    Bump libe for HyperDex

--- libe.rb
+++ libe.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libe < Formula
   homepage 'http://hyperdex.org'
-  url 'http://hyperdex.org/src/libe-0.5.0.tar.gz'
-  sha1 '77c4ffc7472795c63baac12b5488b95b993f8dfe'
+  url 'http://hyperdex.org/src/libe-0.6.0.tar.gz'
+  sha1 '4973b3820166d6e055492ae3ac6a2c149093c2a5'
 
   depends_on 'autoconf'
   depends_on 'automake'
